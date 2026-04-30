87d00164a51ee97ace2add1220a39f5bbe0bc410
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-04-30 19:41:12 +0000

    feat: add port logging for better debugging in cloud environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(9000);
+  const port = process.env.PORT || 9000;
+  await app.listen(port);
+  Logger.log(`🚀 Servidor corriendo en el puerto: ${port}`, 'Bootstrap');
 }
 bootstrap();
