ceefd5909183ca0360628478988a79195b3a9365
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-04-30 19:53:33 +0000

    chore: move to port 3000 to avoid system port conflicts

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 9000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Servidor corriendo en el puerto: ${port}`, 'Bootstrap');
 }
